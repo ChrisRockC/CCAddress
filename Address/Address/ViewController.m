e1fb2532b7e2aa117cb6ea2dfbb3ec90d898a6e4
kakukeme <kakukeme@163.com>
2016-06-08 15:31:31 +0800

    init

---
+++ Address/Address/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Address
+//
+//  Created by CC on 16/6/8.
+//  Copyright © 2016年 CC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
